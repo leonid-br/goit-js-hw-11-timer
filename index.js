fd8fe11fdc89648593145efe2257612976691a7c
unknown <74738757+leonid-br@users.noreply.github.com>
2021-06-10 01:56:24 +0300

    корректировки

--- index.js
+++ index.js
@@ -32,10 +32,16 @@ const timer = new CountdownTimer({
 
 timer.start();
 
+function pad(value) {
+  return String(value).padStart(2, '0');
+};
+
 function getTimeComponents(time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
   return { days, hours, mins, secs };
-}
+};
